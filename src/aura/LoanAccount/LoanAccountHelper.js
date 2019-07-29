93f9240879041ba40340bcc0f951e20f1109eb3e
MeghaGoelMirketa <megha.goel@mirketa.com>
2019-07-29 18:25:58 +0530

    Fixes for namespace issue

--- src/aura/LoanAccount/LoanAccountHelper.js
+++ src/aura/LoanAccount/LoanAccountHelper.js
@@ -9,7 +9,13 @@
                 $A.util.removeClass(spinner, "slds-show");
                 
                 $A.util.addClass(spinner, "slds-hide");
-                component.set("v.FinancialAccountList",response.getReturnValue())
+                var list=JSON.stringify(response.getReturnValue());
+                console.log(list.search("Finsol__"))
+             var list1= list.replace("Finsol__","")
+              console.log(list1)
+                component.set("v.FinancialAccountList",JSON.parse(list1))
+                console.log(JSON.stringify(component.get("v.FinancialAccountList")))
+               // component.set("v.FinancialAccountList",response.getReturnValue())
             }
             else if (state === "ERROR") {
                  var spinner = component.find("mySpinner");
