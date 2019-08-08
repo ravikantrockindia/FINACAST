b9607d65d96ad98b8ba99c9c5bd42f491fe50fc2
Ghanshyam Bhatt <Ghanshyam.Bhatt@sunpowercorp.com>
2019-08-08 14:34:00 +0530

    Revert "sahil bug changes"
    This reverts commit bcb5a3fa1726f66ec054fddeecbc6f520dfecc0d.

--- src/aura/Mybalancesheet/MybalancesheetController.js
+++ src/aura/Mybalancesheet/MybalancesheetController.js
@@ -2,7 +2,6 @@
     doInit : function(component, event, helper) {               
         var jsonResponseJs;  
         var a= component.get("v.ids");
-        component.set("v.isClickable",false);
          
         var action = component.get("c.getData");
         action.setParams({
