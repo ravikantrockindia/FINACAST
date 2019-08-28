80f8ec2f2075c0262e9820bf9658fb7608cb8e36
Ravikant <ravikant@mirketa.com>
2019-08-28 12:56:10 +0530

    Show and Edit Namespace

--- src/aura/ShowClientProfile/ShowClientProfileController.js
+++ src/aura/ShowClientProfile/ShowClientProfileController.js
@@ -1,18 +1,28 @@
 ({	
     doInit:function(component,event,helper){
-    helper.fetchImage(component,event,helper);
-},
+        var action = component.get("c.getNameSpace");
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            
+            if( state === 'SUCCESS') {
+                component.set("v.NameSpace", response.getReturnValue());
+            } 
+        });
+       
+        helper.fetchImage(component,event,helper);
+         $A.enqueueAction(action);
+    },
     itemsChange:function(component,event,helper){
-    helper.fetchImage(component,event,helper);
-},
-	handleProfile : function(component, event, helper) {
-		component.set("v.isEditTrue",true);
+        helper.fetchImage(component,event,helper);
+    },
+    handleProfile : function(component, event, helper) {
+        component.set("v.isEditTrue",true);
         component.set("v.isImageAvailable",true);
         var cmpTarget = component.find('change');
-       	$A.util.removeClass(cmpTarget, "slds-hide"); 
-	},
+        $A.util.removeClass(cmpTarget, "slds-hide"); 
+    },
     closeModel:function(component,event,helper){
-    component.set("v.isEditTrue",false);
-    component.set("v.isImageAvailable",false);    
-}
+        component.set("v.isEditTrue",false);
+        component.set("v.isImageAvailable",false);    
+    }
 })
