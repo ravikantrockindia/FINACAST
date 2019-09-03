1b2925c049ec499b3889e383dafe24707f0bce21
MeghaGoelMirketa <megha.goel@mirketa.com>
2019-09-03 21:49:31 +0530

    Revert "FINA-801 Fields added to demographic"
    This reverts commit eec824b4d482b68775580fd7719b240a3a3cf001.

--- src/aura/EditClientProfile/EditClientProfileController.js
+++ src/aura/EditClientProfile/EditClientProfileController.js
@@ -1,55 +1,4 @@
 ({	 
-    doInit:function(component,event,helper){
-        try{
-            
-        var recordId=component.get("v.recordId");
-            console.log(recordId);
-          //  alert(recordId);
-        if(!($A.util.isUndefinedOrNull(recordId)&&recordId=="")){
-         var action=component.get("c.getContact");
-                action.setParams({ recordId : component.get("v.recordId")});
-                
-                // Create a callback that is executed after 
-                // the server-side action returns
-                action.setCallback(this, function(response) {
-                    var state = response.getState();
-                 //    alert(state)
-                    if (state === "SUCCESS") {
-                        console.log(response.getReturnValue())
-                        component.find("dob").set("v.value", response.getReturnValue()[0].Birthdate);
-                                                component.find("email").set("v.value", response.getReturnValue()[0].Email);
-                        component.find("phone").set("v.value", response.getReturnValue()[0].Phone);
-                        component.find("gender").set("v.value",response.getReturnValue()[0].FinServ__Gender__c)
-
-                    }
-                    
-                    else if (state === "INCOMPLETE") {
-                        // do something
-                    }
-                        else if (state === "ERROR") {
-                           
-                            var errors = response.getError();
-                            if (errors) {
-                                if (errors[0] && errors[0].message) {
-                                    console.log("Error message: " + 
-                                                errors[0].message);
-                                }
-                            } else {
-                                console.log("Unknown error");
-                            }
-                        }
-                });
-                // A client-side action could cause multiple events, 
-                // which could trigger other events and 
-                // other server-side action calls.
-                // $A.enqueueAction adds the server-side action to the queue.
-                $A.enqueueAction(action);
-        }
-        }
-        catch(e){
-            console.log(e.message)
-        }
-    },
     handleCancel : function(component, event, helper) {
         component.set("v.isEditTrue",false);
         component.set("v.isImageAvailable",false); 
@@ -66,54 +15,7 @@
          
     },
     handleSuccess:function(cmp,event,helper){
-        try{
-         var action=cmp.get("c.updateContact");
-          //  alert('abc')
-                action.setParams({ recordId : cmp.get("v.recordId"),
-                                  phone: cmp.find("phone").get("v.value"),
-                                  email: cmp.find("email").get("v.value"),
-                                   gender: cmp.find("gender").get("v.value"),
-                                  dob: cmp.find("dob").get("v.value")
-                                 });
-                
-                // Create a callback that is executed after 
-                // the server-side action returns
-                action.setCallback(this, function(response) {
-                    var state = response.getState();
-                    // alert(state)
-                    if (state === "SUCCESS") {
-                        
-                        
-                    }
-                    
-                    else if (state === "INCOMPLETE") {
-                        // do something
-                    }
-                        else if (state === "ERROR") {
-                            //$A.util.removeClass(spinner, "slds-show");
-                            
-                            //$A.util.addClass(spinner, "slds-hide");
-                            var errors = response.getError();
-                            if (errors) {
-                                if (errors[0] && errors[0].message) {
-                                    console.log("Error message: " + 
-                                                errors[0].message);
-                                }
-                            } else {
-                                console.log("Unknown error");
-                            }
-                        }
-                });
-                // A client-side action could cause multiple events, 
-                // which could trigger other events and 
-                // other server-side action calls.
-                // $A.enqueueAction adds the server-side action to the queue.
-                $A.enqueueAction(action);
-                    cmp.set("v.isEditTrue", false);  
-
-        }catch(e){
-            console.log(e.message);
-        }
+        cmp.set("v.isEditTrue", false);  
     },
     handleSubmit: function(component,event,helper){
         var isValidate=helper.validateData(component,event)
@@ -154,8 +56,5 @@
         else {
             alert('Please Select a Valid File');
         }
-    },
-    itemsChange:function(component,event,helper){
-        helper.uploadHelper(component,event);
     },
 })
