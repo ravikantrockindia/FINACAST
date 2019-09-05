15a0b76f78450a4f77ba7dd52032dc3a8db51001
Ghanshyam Bhatt <Ghanshyam.Bhatt@sunpowercorp.com>
2019-09-05 22:37:43 +0530

    Sahil Code Improvement

--- src/aura/ClientDetail/ClientDetailController.js
+++ src/aura/ClientDetail/ClientDetailController.js
@@ -1,20 +1,19 @@
 ({
 	doInit : function(component, event, helper) {    
-         var action1=component.get('c.getNamespace');
-        console.log('id hello----------'+component.get('v.recordId'));
-        action1.setCallback(this, function(response) {
+         var action=component.get('c.getNamespace');
+        action.setCallback(this, function(response) {
             var state = response.getState();
             if (state === "SUCCESS") {
                 component.set("v.NameSpace", response.getReturnValue());
                 var ab =response.getReturnValue();
-                console.log('MOna'+ab);
+                 
                 var eve = $A.get("e.c:changeclientevent");
-        eve.setParams({"idclient":component.get('v.recordId'),
+        		eve.setParams({"idclient":component.get('v.recordId'),
                        "NameSpace":component.get('v.NameSpace')
                       }); 
-        eve.fire();
+        		eve.fire();
             }           
         });
-        $A.enqueueAction(action1);
+        $A.enqueueAction(action);
 	}
 })
